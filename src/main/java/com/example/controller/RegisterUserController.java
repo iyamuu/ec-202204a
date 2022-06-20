1704091e5f897218c050151d3dc4b6c4cb5d8d64
takattty <r9r_celtics_t9t@icloud.com>
2022-06-20 15:05:24 +0900

    modify tag name and add post routing

--- src/main/java/com/example/controller/RegisterUserController.java
+++ src/main/java/com/example/controller/RegisterUserController.java
@@ -2,14 +2,21 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/user")
 public class RegisterUserController {
 
-	@GetMapping("/")
+	@GetMapping("/sighup")
 	public String toInsert() {
 		return "sighup"; 
+	}
+	
+	@PostMapping("/insert")
+	public String insert() {
+		System.out.println("hey");
+		return "redirect:/user/sighup";
 	}
 }
