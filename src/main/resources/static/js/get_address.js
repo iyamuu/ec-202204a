ddb24bf494553d536404bfb76eac65ca45f4c3c6
takattty <r9r_celtics_t9t@icloud.com>
2022-06-20 16:15:45 +0900

    get address from zipcode

---
+++ src/main/resources/static/js/get_address.js
@@ -0,0 +1,23 @@
+"use strict";
+
+$(function() {
+	$("#zipcode").blur(function() {
+		$.ajax({
+            url: "http://zipcoda.net/api",
+            dataType: "jsonp",
+            data: {
+                zipcode: $("#zipcode").val(),
+            },
+            async: true,
+        })
+            .done(function (data) {
+                $("#address").val(data.items[0].components.join(""));
+            })
+            .fail(function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("正しい情報を得られませんでした");
+                console.log("XMLHttpRequest : " + XMLHttpRequest);
+                console.log("textStatus : " + textStatus);
+                console.log("errorThrown : " + errorThrown);
+            });
+	})
+})
