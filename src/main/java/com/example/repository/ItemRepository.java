9cc3fdcea083ec97b2af8f6e92e1cf5300957dc8
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-06-21 15:37:08 +0900

    確認用のsystem.out.printlnを削除

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -42,7 +42,6 @@ public class ItemRepository {
 	 * @return 全ての商品情報
 	 */
 	public List<Item> findAll(String sort) {
-		System.out.println("引数の中身" + sort);
 		
 		String sql = " SELECT id, name, description, price_m, price_l, image_path, deleted FROM items ORDER BY price_m";
 		if(sort != null && sort.equals("DESC")) {
