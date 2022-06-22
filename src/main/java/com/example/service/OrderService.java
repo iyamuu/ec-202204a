763f9ddef1ec344bcd0b8079b2a1152ba16818cf
takattty <r9r_celtics_t9t@icloud.com>
2022-06-22 12:31:45 +0900

    modify update method to save order

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +14,31 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
-	public Order showOrder(Integer orderId) {
+	public Order showOrder(Integer userId) {
 		Order order = new Order();
 		try {
-			order = orderRepository.findById(orderId).get(0);
+			order = orderRepository.findByUserIdAndStatus(userId, 0).get(0);
 		} catch (IndexOutOfBoundsException e) {
 			order = null;
 		}
 		return order;
 	}
 
-	public void update(Order order) {
-		// ユーザーのIDとstatusが0のorder情報を取得する
-		// 今ユーザー入力した情報と上の情報を合わせてリポジトリに投げる
-		orderRepository.save(order);
+	public void update(Integer userId, Order destinationOrder) {
+		Order orderInCart = orderRepository.findByUserIdAndStatus(userId, 0).get(0);
+		
+		orderInCart.setUserId(userId);
+		orderInCart.setOrderDate(new Date());
+		orderInCart.setStatus(destinationOrder.getPaymentMethod());
+		
+		orderInCart.setDestinationName(destinationOrder.getDestinationName());
+		orderInCart.setDestinationEmail(destinationOrder.getDestinationEmail());
+		orderInCart.setDestinationZipCode(destinationOrder.getDestinationZipCode());
+		orderInCart.setDestinationAddress(destinationOrder.getDestinationAddress());
+		orderInCart.setDestinationTel(destinationOrder.getDestinationTel());
+		orderInCart.setDeliveryTime(destinationOrder.getDeliveryTime());
+		orderInCart.setPaymentMethod(destinationOrder.getPaymentMethod());
+				
+		orderRepository.save(orderInCart);
 	}
 }
