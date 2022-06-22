360ec32bd6e462c6117b5afeb18f7301cfcde162
tarayama <tarayama0302@gmail.com>
2022-06-22 16:19:28 +0900

    カート機能の表示追加バグの修正

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -43,7 +43,7 @@ public class OrderRepository {
 			if (nowOrderId != beforeOrderId) {
 				Order order = new Order();
 				order.setId(nowOrderId);
-				order.setUserId(null);
+				order.setUserId(rs.getInt("user_id"));
 				order.setStatus(rs.getInt("status"));
 				order.setTotalPrice(rs.getInt("total_price"));
 				order.setOrderDate(rs.getDate("order_date"));
@@ -102,7 +102,8 @@ public class OrderRepository {
 				orderItemList.add(orderItem);
 			}
 			
-			if (rs.getInt("order_item_id") == nowOrderItemId) {
+//			if (beforeOrderItemId != nowOrderItemId & rs.getInt("order_item_id") == nowOrderItemId) {
+			if (rs.getInt("ot_id") != 0) {
 				OrderTopping orderTopping = new OrderTopping();
 				orderTopping.setId(rs.getInt("ot_id"));
 				orderTopping.setToppingId(rs.getInt("topping_id"));
