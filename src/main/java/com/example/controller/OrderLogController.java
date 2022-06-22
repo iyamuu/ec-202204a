72ff4c34c42f6d77a1e923679f0b163b37f0f834
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-06-22 10:29:38 +0900

    注文履歴のデータをコントローラーで受け取る

---
+++ src/main/java/com/example/controller/OrderLogController.java
@@ -0,0 +1,44 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Order;
+import com.example.service.OrderLogService;
+
+/**
+ * 注文履歴を表示するコントローラー.
+ * 
+ * @author isodakeisuke
+ */
+@Controller
+@RequestMapping("/log")
+public class OrderLogController {
+
+	@Autowired
+	private OrderLogService orderHistoryService;
+	
+	/**
+	 * 注文履歴を表示します.
+	 * 
+	 * @param userId ユーザー	ID
+	 * @param model モデル
+	 * @return 注文履歴画面
+	 */
+	@RequestMapping("")
+	public String index(Integer userId, Model model) {
+		
+//		List<Order> orderList = orderHistoryService.showOrderHistory(userId);
+		List<Order> orderList = orderHistoryService.showOrderHistory(1);
+		for(Order order : orderList) {
+			System.out.println(order.toString());
+		}
+		model.addAttribute("orderList", orderList);
+		
+		return "order_log";
+	}
+}
---
+++ src/main/java/com/example/service/OrderLogService.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Order;
+import com.example.repository.OrderRepository;
+
+/**
+ * 注文履歴を管理するサービス
+ * @author isodakeisuke
+ *
+ */
+@Service
+public class OrderLogService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	/**
+	 * 注文履歴を表示します.
+	 * @param userId ユーザーID
+	 * @return 注文履歴一覧
+	 */
+	public List<Order> showOrderHistory(Integer userId) {
+		
+		List<Order> orderList = orderRepository.findLogByUserId(userId);
+		return orderList;
+	}
+}
