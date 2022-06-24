e63b8e9e73194960df6da8f1d53c6bed142cd85f
takattty <r9r_celtics_t9t@icloud.com>
2022-06-24 11:05:21 +0900

    remove comment out

--- src/main/resources/static/js/get_address.js
+++ src/main/resources/static/js/get_address.js
@@ -15,8 +15,6 @@ $(function () {
                     $("#address").val(data.items[0].components.join(""));
                 })
                 .fail(function (XMLHttpRequest, textStatus, errorThrown) {
-                    // ここのエラー処理を考える
-                    // alert("正しい情報を得られませんでした");
                     console.log("XMLHttpRequest : " + XMLHttpRequest);
                     console.log("textStatus : " + textStatus);
                     console.log("errorThrown : " + errorThrown);
