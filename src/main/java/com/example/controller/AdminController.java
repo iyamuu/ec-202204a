b6dce76045d6283c1d0e9c3904e311724ac4b049
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-06-24 16:08:31 +0900

    商品の購入数を多い順でコントローラーに渡す（linkedHashMapの形式

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.example.controller;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.LoginUser;
@@ -19,10 +22,27 @@ public class AdminController {
 	@Autowired
 	private AdminService adminService;
 
+	/**
+	 * 管理者画面を表示します.
+	 * 
+	 * @param loginuser ユーザー情報
+	 * @return 管理者画面
+	 */
 	@RequestMapping("")
-	public String index(@AuthenticationPrincipal LoginUser loginuser) {
+	public String index(@AuthenticationPrincipal LoginUser loginuser, Model model) {
 
+		// 注文された数のカウントをします
 		Map<String, Integer> purchasedMap = adminService.showPurchasedCount();
+		
+//		List<String> itemNameSortList = new ArrayList<>();
+//		List<Integer> itemCountSortList = new ArrayList<>();
+//		System.out.println("マップのサイズ" + purchasedMap.size());
+//		
+		
+		
+		// 注文数のカウントはここまで
+		
+		model.addAttribute("itemCountSortList");
 
 		if (isAdmin(loginuser)) {
 			return "/admin";
--- src/main/java/com/example/service/AdminService.java
+++ src/main/java/com/example/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,13 +26,15 @@ public class AdminService {
 	/**
 	 * 購入された商品の個数を表示します.
 	 * 
-	 * @return 商品名とその個数のMap
+	 * @return 商品名とその個数のMap（購入数の多い順に並び替え）
+	 * 
 	 */
 	public Map<String, Integer> showPurchasedCount() {
 		
 		List<Order> orderList = orderRepository.findOrdered();
 		List<Item> itemList = itemRepository.findAll(null);
 		
+		// 商品名と購入数が入ったmapを作成
 		Map<String, Integer> purchasedMap = new HashMap<>();
 		
 		for(Item item: itemList) {
@@ -50,20 +53,27 @@ public class AdminService {
 			}
 		}
 		
-		Map<String, Integer> purchasedSortMap = new HashMap<>();
-		
-		for(String itemName: purchasedMap.keySet()) {
-		}
-		
-		for(String itemName: purchasedMap.keySet()) {
-			Integer itemCount = purchasedMap.get(itemName);
-			if(itemCount >= 0) {
-				System.out.println(itemName + "の購入数は" + itemCount + "です");				
+		// mapを購入数の多い順に並び変えて新しいmapを作成
+		Map<String, Integer> purchasedSortMap = new LinkedHashMap<>();
+		int purchasedMapLenght = purchasedMap.size();
+
+		for (int i = 0; i < purchasedMapLenght; i++) {
+			String maxPurchasedName = null;
+			Integer maxPurchasedCount = null;
+
+			for (String itemName : purchasedMap.keySet()) {
+				if (maxPurchasedCount == null || purchasedMap.get(itemName) > maxPurchasedCount) {
+					maxPurchasedName = itemName;
+					maxPurchasedCount = purchasedMap.get(itemName);
+					
+					System.out.print("*");
+				} 
 			}
+			
+			purchasedSortMap.put(maxPurchasedName, maxPurchasedCount);
+			purchasedMap.remove(maxPurchasedName);
 		}
 		
-		
-		
-		return purchasedMap;
+		return purchasedSortMap;
 	}
 }
